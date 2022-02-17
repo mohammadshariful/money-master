9909d93d8ecb0916b329e73dcca7bd4851d3ac20
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-02-17 09:04:33 +0600

    error handing done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-// selects element
-const inputIncomeField = document.getElementById("input-income");
-const inputFoodField = document.getElementById("input-food");
-const inputRentField = document.getElementById("input-rent");
-const inputClothesField = document.getElementById("input-clothes");
-const inputPersantageField = document.getElementById("persantage-number");
 // common function
 
 // get any element
@@ -18,28 +12,87 @@ function getInputValue(element) {
   inputField.value = "";
   return inputValue;
 }
-// total const
-function costCalc() {
-  const foodCost = getInputValue("input-food");
-  const rentCost = getInputValue("input-rent");
-  const clothesCost = getInputValue("input-clothes");
-  const totalCost = foodCost + rentCost + clothesCost;
-  return totalCost;
+
+// error msg function
+function errorMsg(errId, state) {
+  getElement(errId).style.display = state;
 }
 
 // form added addEventListener
 const form = document.querySelector("form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  const totalCost = costCalc();
-  updateBalance(totalCost);
+
+  const income = getInputValue("input-income");
+  const foodCost = getInputValue("input-food");
+  const rentCost = getInputValue("input-rent");
+  const clothesCost = getInputValue("input-clothes");
+  // conditional checking
+  if (income < 0 || foodCost < 0 || rentCost < 0 || clothesCost < 0) {
+    errorMsg("error-one", "block");
+    errorMsg("error-two", "none");
+  } else if (
+    isNaN(income) ||
+    isNaN(foodCost) ||
+    isNaN(rentCost) ||
+    isNaN(clothesCost)
+  ) {
+    errorMsg("error-one", "none");
+    errorMsg("error-two", "block");
+  } else {
+    errorMsg("error-one", "none");
+    errorMsg("error-two", "none");
+    const totalCost = foodCost + rentCost + clothesCost;
+    updateBalance(income, totalCost);
+  }
 });
 // update balance
-
-function updateBalance(totalCost) {
-  const addIncome = getInputValue("input-income");
+function updateBalance(income, totalCost) {
   const balance = getElement("total-balance");
   const expenses = getElement("total-expenses");
-  expenses.innerText = totalCost;
-  balance.innerText = addIncome - totalCost;
+  // conditional checking
+  if (totalCost > income) {
+    errorMsg("error-three", "block");
+  } else {
+    errorMsg("error-three", "none");
+    expenses.innerText = totalCost;
+    balance.innerText = income - totalCost;
+  }
+}
+//
+const savingBtn = getElement("saving-btn");
+// addEventListener for saving buttons
+savingBtn.addEventListener("click", savingBalance);
+function savingBalance() {
+  const savingInput = getInputValue("persantage-number");
+  const totalBalance = getElement("total-balance");
+  const numberOfBalance = parseInt(totalBalance.innerText);
+  const persantage = savingInput / 100;
+  // conditional checking
+
+  if (Number.isNaN(savingInput)) {
+    errorMsg("error-four", "block");
+    errorMsg("error-five", "none");
+  } else if (savingInput < 0 || savingInput > 100) {
+    errorMsg("error-four", "none");
+    errorMsg("error-five", "block");
+  } else {
+    errorMsg("error-four", "none");
+    errorMsg("error-five", "none");
+    savingAmount(numberOfBalance, persantage);
+  }
+}
+// saving ammout function
+function savingAmount(balance, persantage) {
+  const savingAmount = getElement("saving-amount");
+  const ramainingBalance = getElement("remaing-balance");
+  const persantageBalance = balance * persantage;
+  // conditional checking
+  if (persantageBalance >= balance) {
+    errorMsg("error-six", "block");
+  } else {
+    errorMsg("error-six", "none");
+    savingAmount.innerText = persantageBalance.toFixed(2);
+    ramainingBalance.innerText = balance - persantageBalance;
+  }
 }
